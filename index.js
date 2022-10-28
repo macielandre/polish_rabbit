b14220fe86c47b602ea62812d83b954a4fcdb03b
André Maciel <56964039+macielandre@users.noreply.github.com>
2022-10-28 00:25:51 -0300

    using inheritance

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
-const amqp = require('amqplib')
-
-class Rabbitmq {
-    constructor({ queue, user, password, host, port, uri = null, options = {} }) {
-        this.connection = null
-        this.channel = null
-        this.queue = queue
-        this.uri = uri ? uri : `amqp://${user}:${password}@${host}:${port}`
-        this.options = options
-    }
-
-    async connect() {
-        this.connection = await amqp.connect(this.uri, this.options)
-        this.channel =  await this.connection.createChannel()
-
-        await this.channel.assertQueue(this.queue, { durable: false })
-
-        console.log('[Rabbitmq] Connection succeded')
-    }
-
-    enqueue(message, options = {}) {
-        if (typeof message !== 'string') {
-            message = JSON.stringify(message)
-        }
-
-        const messageBuffer = Buffer.from(message)
-
-        this.channel.sendToQueue(this.queue, messageBuffer, options)
-
-        console.log('[Rabbitmq] Message enqueued')
-    }
-
-    async dequeue(executableFn, options = {}) {
-        await this.channel.consume(this.queue, async bufferMessage => {
-            const message = bufferMessage.content.toString()
-
-            executableFn(message)
-        }, options)
-    }
-
-    async close() {
-        await this.connection.close()
-    }
+module.exports = { 
+    Connection: require('./lib/connection'),
+    Channel: require('./lib/channel'),
+    ConfirmChanell: require('./lib/confirmChannel')
 }
-
-module.exports = Rabbitmq
