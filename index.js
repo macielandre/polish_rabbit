2b762112861855bc6034e7c560d0835ce1f53fac
macielandre <maciel.andre@live.com>
2023-07-23 22:14:32 -0300

    accepting vhost and changing uri logic

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Connection = require("./lib/connection")
 const Channel = require("./lib/channel")
+const Queue = require("./lib/queue")
 
 module.exports = {
   Connection,
   Channel,
+  Queue,
 }
